454324b0726c5a08cb00729a6fde038272deb6f5
candytuft <vishnyc3@gmail.com>
2021-02-08 05:56:02 -0500

    Adds Photobook Model, route and controller

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Photobook:
+    def __init__(self, name, theme, description):
+        self.name = name
+        self.theme = theme
+        self.description = description
+        
+photobooks = [
+    Photobook('Art', 'Color Block', 'A collection of all the Art I made in 2019'),
+    Photobook('Best Friend and I', 'Minimal Black', 'All the crazy fun memories with my best friend so far'),
+    Photobook('Lavender', 'Minimal White', 'Photos captured based off of everything Lavender')
+]       
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def photobooks(request):
+    return render (request, 'photobooks/index.html', { 'photobooks': photobooks })
