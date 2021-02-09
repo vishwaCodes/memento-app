975dbbe072f2933764c0acfe3385924cf79f12f6
candytuft <vishnyc3@gmail.com>
2021-02-09 02:07:46 -0500

    Adds a Share Section for the User, adds more divs to style, configures static files

--- main_app/views.py
+++ main_app/views.py
@@ -19,8 +19,8 @@ def gallery(request):
     return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
-    # photo = Photo.objects.get(id=pk)
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
 
 def addPhoto(request):
     return render(request, 'photos/add.html')
--- photobook_project/urls.py
+++ photobook_project/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
-from django.conf.urls.static  import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +24,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
