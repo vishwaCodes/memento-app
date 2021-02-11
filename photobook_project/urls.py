3a0cc3e02f30a183cbbad03181b14dc9c3e14ba8
candytuft <vishnyc3@gmail.com>
2021-02-10 20:01:38 -0500

    Configures URL Patterns

--- photobook_project/urls.py
+++ photobook_project/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('', include('main_app.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
